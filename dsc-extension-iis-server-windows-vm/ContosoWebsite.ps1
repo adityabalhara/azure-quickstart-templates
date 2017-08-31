51e131339529806ee6db51b3cf4f2aeb9e92ecb1
adityabalhara <aditya.balhara@gmail.com>
2017-08-31 15:13:23 +0530

    Add files via upload

---
+++ dsc-extension-iis-server-windows-vm/ContosoWebsite.ps1
@@ -0,0 +1,33 @@
+Configuration ContosoWebsite
+{
+  param ($MachineName)
+
+  Node $MachineName
+  {
+    #Install the IIS Role
+    WindowsFeature IIS
+    {
+      Ensure = “Present”
+      Name = “Web-Server”
+    }
+
+    #Install ASP.NET 4.5
+    WindowsFeature ASP
+    {
+      Ensure = “Present”
+      Name = “Web-Asp-Net45”
+    }
+    #Install ASP.NET 3.5
+    WindowsFeature ASP
+    {
+      Ensure = “Present”
+      Name = “Web-Asp-Net35”
+    }
+
+     WindowsFeature WebServerManagementConsole
+    {
+        Name = "Web-Mgmt-Console"
+        Ensure = "Present"
+    }
+  }
+} 
